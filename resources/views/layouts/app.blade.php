397332da379ef487fa9df620e33a03aad0b1dff4
Evgniy Nakoneschniy <e.nakoneschniy@gmail.com>
2016-10-19 09:45:41 +0300

    redirect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,10 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type='text/javascript'>
+        if(window.innerWidth<600)
+            location.href='http://m.ukrlogika.com/';
+    </script>
 </head>
 <body>
 <div class="wrapper">
