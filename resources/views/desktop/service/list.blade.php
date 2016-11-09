0634790333f0f2432ac7f1d9d0ee42b0cbe531d0
Evgeniy Nakoneschniy <e.nakoneschniy@gmail.com>
2016-11-09 16:08:30 +0200

    last

--- resources/views/desktop/service/list.blade.php
+++ resources/views/desktop/service/list.blade.php
@@ -1,60 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row widget">
-        <div class="block_leftbar">
-            <div class="img">
-                <i class="ico_consult abs_center sprites"></i>
-            </div>
-            <p>{{$consultation->getTitle()}}</p>
-            <ul class="radio_btns">
-                @foreach($consultation->getChildrenCategories() as $category)
-                    <li class="">
-                        <input class="radio" id="radio_consult{{$loop->index}}" name="radio_consult" type="radio" @if($loop->first) checked @endif>
-                        <label tabindex="4" for="radio_consult{{$loop->index}}" class="radio-label">{{$category->getTitle()}}</label>
-                    </li>
-                @endforeach
-            </ul>
-        </div><!-- /.block_leftbar -->
-        <div class="tab tabs_consult_child">
-            <ul class="tabs tabs_consult ">
-                @foreach($consultation->getChildrenCategories() as $children)
-                    @if($loop->first)
-                        @foreach($children->getChildrenCategories() as $cat)
-                            <li><a href="#">{{$cat->getTitle()}}</a></li>
-                        @endforeach
-                    @endif
-                @endforeach
-            </ul>
-            <br>
-            <div class="tab_content">
-                @foreach($consultation->getChildrenCategories() as $children)
-                    @foreach($children->getChildrenCategories() as $cat)
-                        <div class="tabs_item">
-                            <div class="counterSlides">
-                                <span class="currentSlide">1</span>
-                                <span class="separatorSlide">/</span>
-                                <span class="totalSlide">1</span>
-                            </div>
-                            <div class="carousel_items">
-                                @foreach($cat->news as $service)
-                                    <a href="{{url('/consultation',[$cat->id, $service->id])}}" class="item">
-                                        <div class="img">
-                                            <img src="{{$service->previewImage()}}" alt="">
-                                        </div>
-                                        <div class="info">
-                                            <p class="person_name">{{$service->getTitle()}}</p>
-                                            <p class="person_post">{{$service->doctor->getName()}}</p>
-                                        </div>
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
+    @if(count($consultation['specialties']) > 0)
+        <div class="row widget">
+            <div class="block_leftbar">
+                <div class="img">
+                    <i class="ico_consult abs_center sprites"></i>
+                </div>
+                <p>@lang('index.consultation_left')</p>
+
+                <ul class="radio_btns cnsult_list">
+                    @foreach($consultation['specialties'] as $key_spec => $specialty)
+                        <li data-tabs="{{$key_spec}}">
+                            <input class="radio" id="radio_consult{{$loop->index}}" name="radio_consult" type="radio" @if($loop->first) checked @endif>
+                            <label tabindex="4" for="radio_consult{{$loop->index}}" class="radio-label consult">{{$specialty['title']}}</label>
+                        </li>
                     @endforeach
-                @endforeach
+                </ul>
+            </div><!-- /.block_leftbar -->
+            <div class="tab tabs_consult_child">
+                <ul class="tabs tabs_consult">
+                    @foreach($consultation['specialties'] as $key => $specialty)
+                        @if($loop->first)
+                            @foreach($specialty['jobs'] as $job)
+                                <li class="{{$key}}"><a href="#">{{$job['title']}}</a></li>
+                            @endforeach
+                        @else
+                            @foreach($specialty['jobs'] as $job)
+                                <li class="{{$key}}" style="display:none"><a href="#">{{$job['title']}}</a></li>
+                            @endforeach
+                        @endif
+                    @endforeach
+                </ul> <!-- / tabs -->
+                <div class="tab_content">
+                    @foreach($consultation['specialties'] as $key => $specialty)
+                        @if($loop->first)
+                            @foreach($specialty['jobs'] as $keyJob => $job)
+                                <div class="tabs_item">
+                                    <div class="counterSlides">
+                                        <span class="currentSlide">1</span>
+                                        <span class="separatorSlide">/</span>
+                                        <span class="totalSlide">1</span>
+                                    </div>
+                                    <div class="carousel_items">
+                                        @foreach($job['doctors'] as $key => $doctor)
+                                            <a href="{{url('/consultation', [$keyJob, $key])}}" class="item">
+                                                <div class="img">
+                                                    <img src="{{$doctor->previewImage()}}" alt="">
+                                                </div>
+                                                <div class="info">
+                                                    <p class="person_name">{{$doctor->getName()}}</p>
+                                                    <p class="person_post">{{$doctor->getQualify()}}</p>
+                                                </div>
+                                            </a>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            @foreach($specialty['jobs'] as $keyJob => $job)
+                                <div class="tabs_item">
+                                    <div class="counterSlides">
+                                        <span class="currentSlide">1</span>
+                                        <span class="separatorSlide">/</span>
+                                        <span class="totalSlide">1</span>
+                                    </div>
+                                    <div class="carousel_items">
+                                        @foreach($job['doctors'] as $key => $doctor)
+                                            <a href="{{url('/consultation', [$keyJob, $key])}}" class="item">
+                                                <div class="img">
+                                                    <img src="{{$doctor->previewImage()}}" alt="">
+                                                </div>
+                                                <div class="info">
+                                                    <p class="person_name">{{$doctor->getName()}}</p>
+                                                    <p class="person_post">{{$doctor->getQualify()}}</p>
+                                                </div>
+                                            </a>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
+    @endif
     @foreach($leftItems as $rootCategory)
         <div class="row widget">
             <div class="block_leftbar">
